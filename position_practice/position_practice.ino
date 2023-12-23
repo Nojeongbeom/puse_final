afadaa1318e1bc2fed4108aba3a1b7d539df615f
NoJeongBeom <wjdqja2001@naver.com>
2023-12-23 16:56:44 +0900

    updated position code

--- position_practice/position_practice.ino
+++ position_practice/position_practice.ino
@@ -35,10 +35,9 @@
   #define DEBUG_SERIAL Serial
   const int DXL_DIR_PIN = 2; // DYNAMIXEL Shield DIR PIN
 #endif
- 
 
-#define MAX_BAUD  5
-const int32_t buad[MAX_BAUD] = {57600, 115200, 1000000, 2000000, 3000000};
+const uint8_t DXL_IDS[] = {1, 2, 3, 4, 5, 6};  // List of Dynamixel IDs
+const float DXL_PROTOCOL_VERSION = 2.0;
 
 Dynamixel2Arduino dxl(DXL_SERIAL, DXL_DIR_PIN);
 
@@ -46,46 +45,38 @@ Dynamixel2Arduino dxl(DXL_SERIAL, DXL_DIR_PIN);
 using namespace ControlTableItem;
 
 void setup() {
-  // put your setup code here, to run once:
-  int8_t index = 0;
-  int8_t found_dynamixel = 0;
+  Serial.begin(9600);
+  dxl.begin(57600);
+  // Set Port Protocol Version. This has to match with DYNAMIXEL protocol version.
+  dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
 
-  // Use UART port of DYNAMIXEL Shield to debug.
-  DEBUG_SERIAL.begin(115200);   //set debugging port baudrate to 115200bps
-  while(!DEBUG_SERIAL);         //Wait until the serial port is opened
-    
-  for(int8_t protocol = 1; protocol < 3; protocol++) {
-    // Set Port Protocol Version. This has to match with DYNAMIXEL protocol version.
-    dxl.setPortProtocolVersion((float)protocol);
-    DEBUG_SERIAL.print("SCAN PROTOCOL ");
-    DEBUG_SERIAL.println(protocol);
-    
-    for(index = 0; index < MAX_BAUD; index++) {
-      // Set Port baudrate.
-      DEBUG_SERIAL.print("SCAN BAUDRATE ");
-      DEBUG_SERIAL.println(buad[index]);
-      dxl.begin(buad[index]);
-      for(int id = 0; id < DXL_BROADCAST_ID; id++) {
-        //iterate until all ID in each buadrate is scanned.
-        if(dxl.ping(id)) {
-          DEBUG_SERIAL.print("ID : ");
-          DEBUG_SERIAL.print(id);
-          DEBUG_SERIAL.print(", Model Number: ");
-          DEBUG_SERIAL.println(dxl.getModelNumber(id));
-          found_dynamixel++;
-        }
-      }
-    }
+  // Get DYNAMIXEL information for each motor
+  for (uint8_t i = 0; i < sizeof(DXL_IDS) / sizeof(DXL_IDS[0]); i++) {
+    dxl.ping(DXL_IDS[i]);
+    dxl.torqueOff(DXL_IDS[i]);
+    dxl.setOperatingMode(DXL_IDS[i], OP_EXTENDED_POSITION);
+    dxl.torqueOn(DXL_IDS[i]);
+    // Limit the maximum velocity in Position Control Mode. Use 0 for Max speed
+    dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_IDS[i], 50);
+    dxl.writeControlTableItem(POSITION_P_GAIN, DXL_IDS[i], 1000);
   }
-  
-  DEBUG_SERIAL.print("Total ");
-  DEBUG_SERIAL.print(found_dynamixel);
-  DEBUG_SERIAL.println(" DYNAMIXEL(s) found!");
-
-  // Set ID
-  //dxl.setID(2, 5); // 뒤에꺼가 기존 번호, 앞에꺼가 새 번호
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (Serial.available()) {
+    float inputValue = Serial.readStringUntil('\n').toFloat();
+    // Set goal position for each motor
+    for (uint8_t i = 0; i < sizeof(DXL_IDS) / sizeof(DXL_IDS[0]); i++) {
+      dxl.setGoalPosition(DXL_IDS[i], inputValue, UNIT_DEGREE);
+    }
+  }
+
+  // Print current positions for each motor
+  for (uint8_t i = 0; i < sizeof(DXL_IDS) / sizeof(DXL_IDS[0]); i++) {
+    Serial.print("Motor ");
+    Serial.print(DXL_IDS[i]);
+    Serial.print(" POSITION DEGREE : ");
+    Serial.println(dxl.getPresentPosition(DXL_IDS[i], UNIT_DEGREE));
+  }
+  Serial.println();  // Add a newline for clarity
 }
